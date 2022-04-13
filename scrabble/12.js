c92572b46a8449259a1cb5b50b781f1beb5fffd7
rconjoe <joe@rconjoe.com>
2022-04-13 11:15:11 -0700

    remove solutions from problems branch

--- scrabble/12.js
+++ scrabble/12.js
@@ -1,19 +1,1 @@
 // Create and print an array containing all the words that end in 'GHTLY'
-
-const fs = require('fs');
-const raw = fs.readFileSync('./sowpods.txt', 'utf8');
-const data = raw.split('\n');
-
-function solution(words) {
-  const result = []
-
-  words.forEach(word => {
-    if (word.endsWith('GHTLY')) {
-      result.push(word)
-    }
-  })
-
-  console.log(result)
-}
-
-solution(data)
