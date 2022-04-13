4a584bc937b6d6ab0c7f146db4382a90e4c0eacd
rconjoe <joe@rconjoe.com>
2022-04-13 11:51:08 -0700

    init arrays problems

---
+++ arrays-and-objects/arrays.js
@@ -0,0 +1,23 @@
+// 7. Create an array named colors that contains five different names of colors as strings.
+
+// 8. Access the first color in the array and print it to the console using console.log()
+
+// 9. Now do the same with the third color in the list. (Remember that array indexes start at 0, not at 1!)
+
+// 10. Write one line of code that changes the value of the last color in the list to "ultraviolet" (overwriting the previous value).
+
+// 11. Create a new variable called fourthColor and set it equal to the fourth color in the list.
+
+// 12. Add another color to the end of the list.
+
+// 13. Add another color to the beginning of the list.
+
+// 14. Print the length of the array to the console with console.log()
+
+// 15. Remove the last color from the end of list, and then print the length of the array to the console one more time.
+
+// 16. Write a for loop to iterate through every color in the array and print each color's value to the console.
+
+// 17. Copying from that loop you just wrote, modify it to print every color's value and every color's index in this format: "3, purple" or "0, blue" etc.
+
+// 18. Create a variable named lastColor that will always point to the last element of the colors array, no matter how many colors are in the list. (Hint: make use of the array's length property for this!)
