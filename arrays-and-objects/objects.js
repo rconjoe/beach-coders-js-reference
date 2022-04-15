868f0cbb67ddaf18af5234473e68f161c895b3d7
ben40d9 <benpaley6@gmail.com>
2022-04-15 12:56:02 -0700

    objects completed up to #26

--- arrays-and-objects/objects.js
+++ arrays-and-objects/objects.js
@@ -1,24 +1,72 @@
 // 19. Pick a penguin from Wikipedia's List of Fictional Penguins and create an object named myPenguin with properties that represent the information listed in each column on that Wikipedia page (for example: the character's name, origin, and author).
+const myPenguin = {
+    name: 'Pablo',
+    origin: 'The Backyardigans',
+    creator: 'Janice Burgess'
+
+}
 
 // 20. Use console.log() to print the penguin's name to the console as part of a welcome message, like "Hello, I'm a penguin and my name is [NAME HERE]!"
+console.log(myPenguin.name)
 
 // 21. Write another line of code that adds a new property to your penguin called canFly and set it to false. (Note: Don't modify your penguin-creation code that you wrote above. Do this step in a separate line of code.)
+myPenguin.canFly = false
+console.log(myPenguin.canFly)
 
 // 22. Add a method to your penguin called chirp that prints to the console: "CHIRP CHIRP! Is this what penguins sound like?" (Note: Again, don't modify your previous code. Do this step by writing a new line of code.)
 // Hint: a function is called a "method" when it's inside an object.
+myPenguin.chirp = "CHIRP CHIRP! Is this what penguins sound like?"
 
 // TO TEST IT, RUN THIS CODE: myPenguin.chirp();
+console.log(myPenguin.chirp)
 
 // 23. Add another method to your penguin called sayHello that prints to the console the same message from step 20 above. This time, be sure to use the "this" keyword to access your penguin's name, so that way if you change its name later, your method still works.
+myPenguin.sayHello = "CHIRP CHIRP! Is this what penguins sound like?"
+function penguin () {
+    console.log(this.name)
+}
+myPenguin.penguin = penguin;
+
+console.log(myPenguin.penguin()) //not sure whats coming up as undefined
 
 // 24. Next, call your penguin's sayHello() method and make sure that it works. (Hint: if you need an example of what it looks like when you call a method of an object, look at console.log() -- that's how you call the log() method of the console object.)
+console.log(myPenguin.sayHello)
 
 // 25. Without modifying any of your previous code, change the penguin's name to "chungus" and then call your penguin's sayHello() function one more time to make sure it still works.
+myPenguin.name = "chungus"
+console.log(myPenguin.name)
+
+// console.log(myPenguin.sayHello)
 
 // 26. Write another method called fly, and inside that method, use an if / else statement to print "I can fly!" to the console if your penguin's canFly property is true, or "No flying for me!" if its canFly property is false.
 
-// 27. Call your penguin's fly() method and make sure it works.
+// let result 
+//     if (myPenguin.canFly = true) {
+//         result = "I can fly!"
+//     } else {
+//         result = "No flying for me!"
+//     }
+//     return result
+// myPenguin.fly = result
 
+// myPenguin.fly = result
+// console.log(myPenguin.fly)//not right logs nothing
+
+
+function someFunction() {
+    if (canFly === true) {
+        return "I can fly!"
+    } else {
+        return "No flying for me!"
+    }
+}
+myPenguin.fly = someFunction;
+
+console.log(myPenguin.fly) //not right (logging "[Function: someFunction]")
+
+
+// 27. Call your penguin's fly() method and make sure it works.
+// console.log(myPenguin.fly)
 // 28. Change the canFly property to true -- again, without modifying any of your previous code.
 
 // 29. Now call your penguin's fly() method again and make sure it works as expected.
