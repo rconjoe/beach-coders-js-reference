033055b22eb04f23de80f3c2fd4f620a9cb36a51
ben40d9 <benpaley6@gmail.com>
2022-04-16 17:54:43 -0700

    completed scrabble #11

--- scrabble/11.js
+++ scrabble/11.js
@@ -1,1 +1,55 @@
 // How many words contain the substring 'TYPE'?
+
+const fs = require('fs')
+
+const data = fs.readFileSync('./sowpods.txt', 'utf-8')
+const wordList = data.split('\n')
+
+function solution(words) {
+    const result = []
+    words.forEach(word => {
+        if (word.match(/TYPE/g)) {
+            result.push(word)
+        }
+    })
+    return result
+}
+
+console.log(solution(wordList)) //mine only shows the array, not the list of arrays
+
+
+// function solution (words) {
+//     words.forEach(function(word){
+//         let i = 0;
+//         while (i < 1 && word.includes('TYPE')) {
+//             console.log(i);
+//             i++;
+//         }
+//     })
+// }
+
+// solution(wordList)
+
+
+
+
+
+
+
+// occurences (data, 'TYPE')
+
+
+
+
+// function solution (words) {
+//     words.forEach(function(word){
+//         let i = 0;
+//         do {
+//             console.log(i);
+//             i++;
+//             //break;
+//         } while (word.includes('TYPE'))
+//     })
+// }
+
+// solution(wordList)
