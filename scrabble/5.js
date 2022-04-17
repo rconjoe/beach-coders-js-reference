5e72871a5edd636fb842e388f5595d2e6ff2dd73
ben40d9 <benpaley6@gmail.com>
2022-04-16 17:53:37 -0700

    completed scrabble #5

--- scrabble/5.js
+++ scrabble/5.js
@@ -1,1 +1,17 @@
+const fs = require('fs')
+
+const data = fs.readFileSync('./sowpods.txt', 'utf-8')
+const wordList = data.split('\n')
+
+
 // What are all of the words that have no E or A and are at least 15 letters long?
+
+function solution (words) {
+    words.forEach(word => {
+        if (!word.includes('E') && !word.includes('A') && word.length >= 15) {
+            console.log(word)
+        }
+    })
+} 
+
+solution(wordList)
