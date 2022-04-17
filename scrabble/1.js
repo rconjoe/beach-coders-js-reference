94216dae149bd884c9cad2638a324cc4d2f2443a
ben40d9 <benpaley6@gmail.com>
2022-04-16 17:53:03 -0700

    completed scrabble #1

--- scrabble/1.js
+++ scrabble/1.js
@@ -1,1 +1,19 @@
 // What are all of the words containing UU?
+const fs = require('fs')
+
+const data = fs.readFileSync('./sowpods.txt', 'utf-8')
+const wordList = data.split('\n')
+
+
+// What are all of the words containing UU?
+
+function solution (words) {
+    words.forEach(function(word) {
+        if (word.includes('UU')) {
+            console.log(word)
+        }
+    })
+} 
+
+solution(wordList)
+
