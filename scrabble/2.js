b97d741484e995b119b5b7c2295fc180a2673a31
ben40d9 <benpaley6@gmail.com>
2022-04-16 17:53:10 -0700

    completed scrabble #2

--- scrabble/2.js
+++ scrabble/2.js
@@ -1,1 +1,31 @@
+const fs = require('fs')
+
+const data = fs.readFileSync('./sowpods.txt', 'utf-8')
+const wordList = data.split('\n')
+
+
 // What are all of the words containing an X and a Y and a Z? 
+
+function solution (words) {
+    words.forEach(function(word){
+        if (word.includes('X') && word.includes('Y') && word.includes('Z')) {
+            console.log(word)
+        }
+    })
+}
+
+solution(wordList)
+
+
+
+
+
+// function solution (words) {
+//     words.forEach(function(word){
+//         if (word.includes('X', 'Y', 'Z')) {
+//             console.log(word)
+//         }
+//     })
+// }
+
+// solution(wordList)
