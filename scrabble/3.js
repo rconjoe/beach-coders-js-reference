f85c204979dde79ae72d148b8071eaf9f4863523
ben40d9 <benpaley6@gmail.com>
2022-04-16 17:53:17 -0700

    completed scrabble #3

--- scrabble/3.js
+++ scrabble/3.js
@@ -1,1 +1,34 @@
+const fs = require('fs')
+
+const data = fs.readFileSync('./sowpods.txt', 'utf-8')
+const wordList = data.split('\n')
+
+
 // What are all of the words containing a Q but not a U?
+
+
+function solution (words) {
+    words.forEach(function(word){
+        if (word.includes('Q') && !word.includes('U')) {
+            console.log(word)
+        } 
+    })
+}
+
+solution(wordList)
+
+
+
+
+
+// const qWord = qWord.includes('Q')
+
+// function solution (words) {
+//     words.forEach(function(word){
+//         if (word.includes('Q') && !word.includes('U')) {
+//             console.log(word)
+//         } 
+//     })
+// }
+
+// solution(wordList)
