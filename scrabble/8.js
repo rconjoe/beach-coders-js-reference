2eb0ad3318fc0b1587e0976fa1a659d734b1f1a4
ben40d9 <benpaley6@gmail.com>
2022-04-16 17:54:09 -0700

    completed scrabble #8

--- scrabble/8.js
+++ scrabble/8.js
@@ -1,1 +1,18 @@
 // What are all of the words with no vowel and not even a Y?
+
+const fs = require('fs')
+
+const data = fs.readFileSync('./sowpods.txt', 'utf-8')
+const wordList = data.split('\n')
+
+
+
+function solution (words) {
+    words.forEach(function(word) {
+        if (!word.includes('E') && !word.includes('A') && !word.includes('I') && !word.includes('O') && !word.includes('U') && !word.includes('Y')) {
+            console.log(word)
+        }
+    })
+} 
+
+solution(wordList)
