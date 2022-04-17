2504bb631fda9c0b74ac0d4d47f822b7ad8e824f
ben40d9 <benpaley6@gmail.com>
2022-04-16 17:53:45 -0700

    completed scrabble #6

--- scrabble/6.js
+++ scrabble/6.js
@@ -1,1 +1,18 @@
 // What are all of the words that have a B and an X and are less than 5 letters long?
+
+const fs = require('fs')
+
+const data = fs.readFileSync('./sowpods.txt', 'utf-8')
+const wordList = data.split('\n')
+
+
+
+function solution (words) {
+    words.forEach(function(word) {
+        if (word.includes('B') && word.includes('X') && word.length <= 4) {
+            console.log(word)
+        }
+    })
+} 
+
+solution(wordList)
