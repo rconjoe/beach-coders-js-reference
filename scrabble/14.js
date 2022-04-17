602a6b8e03292c00e0504eb57da2e1d97e4d24e7
ben40d9 <benpaley6@gmail.com>
2022-04-16 23:20:11 -0700

    notes on 14.js

--- scrabble/14.js
+++ scrabble/14.js
@@ -1,1 +1,8 @@
 // What is the longest word that contains no vowels?
+
+//something like 
+// if (!word.includes('A') && !word.includes('E')) {//....ect
+//     console.log(count of some sort)
+// }
+
+//then indexOf longest but I dont know how to do that
