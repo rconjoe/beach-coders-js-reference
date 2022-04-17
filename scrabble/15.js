9dd42abad43bf292f5531feabdeba3fdee163d25
ben40d9 <benpaley6@gmail.com>
2022-04-16 23:20:30 -0700

    started 15.js

--- scrabble/15.js
+++ scrabble/15.js
@@ -1,1 +1,18 @@
 // Which of the letters Q, X, and Z is the least common?
+
+const fs = require('fs')
+
+const data = fs.readFileSync('./sowpods.txt', 'utf-8')
+const wordList = data.split('\n')
+
+
+
+function solution (words) {
+    words.forEach(function(word) {
+        if (!word.includes('Q')) {
+            console.log(word.count())
+        }
+    })
+} 
+
+solution(wordList)
