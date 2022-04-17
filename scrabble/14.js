2dcbf284a46bc1340295889d6fdb900da03202c6
ben40d9 <benpaley6@gmail.com>
2022-04-17 00:56:10 -0700

    completed 14.js but am confused why I am getting 2 solutions

--- scrabble/14.js
+++ scrabble/14.js
@@ -1,8 +1,41 @@
+const fs = require('fs');
+const raw = fs.readFileSync('./sowpods.txt', 'utf8');
+const data = raw.split('\n');
+
+
 // What is the longest word that contains no vowels?
 
-//something like 
-// if (!word.includes('A') && !word.includes('E')) {//....ect
-//     console.log(count of some sort)
-// }
+// function solution(words) {
+//     var result = []
+//     words.forEach(word => {
+//       if (!word.includes('A') && !word.includes('E') && !word.includes('I') && !word.includes('O') && !word.includes('U')) {
+//         result.push(word)
+//       }
+//     })
+  
+//     console.log(result.reduce((a, b) => a.length >= b.length ? a : b)
+//     )
+//   }
+  
+//   solution(data)//this displays 'GLYCYLS'
 
-//then indexOf longest but I dont know how to do that
+
+
+function solution(words) {
+    var result = []
+    words.forEach(word => {
+      if (!word.includes('A') && !word.includes('E') && !word.includes('I') && !word.includes('O') && !word.includes('U')) {
+        result.push(word)
+      }
+    })
+  
+    console.log(result.reduce((a, b) => a.length > b.length ? a : b)
+    )
+  }
+  
+  solution(data) //this displays 'TSKTSKS', both are 7 letters 
+
+
+
+
+
