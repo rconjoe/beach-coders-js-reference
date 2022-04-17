1c8fa6a2014001330afe1916075a91c14e3b2494
ben40d9 <benpaley6@gmail.com>
2022-04-16 17:54:20 -0700

    completed scrabble #9

--- scrabble/9.js
+++ scrabble/9.js
@@ -1,1 +1,18 @@
 // What are all of the words that have all 5 vowels, in any order?
+
+const fs = require('fs')
+
+const data = fs.readFileSync('./sowpods.txt', 'utf-8')
+const wordList = data.split('\n')
+
+
+
+function solution (words) {
+    words.forEach(function(word) {
+        if (word.includes('E') && word.includes('A') && word.includes('I') && word.includes('O') && word.includes('U')) {
+            console.log(word)
+        }
+    })
+} 
+
+solution(wordList)
