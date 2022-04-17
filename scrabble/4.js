518ef7e1adcbd82891cac8c1e4d67cc55c5e6cda
ben40d9 <benpaley6@gmail.com>
2022-04-16 17:53:27 -0700

    completed scrabble #4

--- scrabble/4.js
+++ scrabble/4.js
@@ -1,2 +1,18 @@
 // What are all of the words that contain the word CAT and are exactly 5 letters long?
 
+// 1. contains cat
+// 2. 5 letters long
+
+const fs = require('fs');
+const raw = fs.readFileSync('./sowpods.txt', 'utf8');
+const data = raw.split('\n');
+
+function solution(words) {
+  words.forEach(word => {
+    if (word.includes('CAT') && word.length === 5) {
+      console.log(word)
+    }
+  })
+}
+
+console.log(solution(data));
