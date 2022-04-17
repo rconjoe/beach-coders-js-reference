16b0f209c82549e1d8502f22045abf6dfc414ae7
ben40d9 <benpaley6@gmail.com>
2022-04-16 17:53:57 -0700

    completed scrabble #7

--- scrabble/7.js
+++ scrabble/7.js
@@ -1,1 +1,17 @@
+const fs = require('fs')
+
+const data = fs.readFileSync('./sowpods.txt', 'utf-8')
+const wordList = data.split('\n')
+
+
 // What are all of the words that start and end with a Y?
+
+function solution (words) {
+    words.forEach(function(word) {
+        if (word.startsWith('Y') || word.endsWith('Y')) {
+            console.log(word)
+        }
+    })
+} 
+
+solution(wordList)
