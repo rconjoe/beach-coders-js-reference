66321c41aa704dd08fb0629e9cd565be64b13335
ben40d9 <benpaley6@gmail.com>
2022-04-18 00:19:14 -0700

    completed 15.js

--- scrabble/15.js
+++ scrabble/15.js
@@ -1,18 +1,43 @@
+const fs = require('fs')
+const text = fs.readFileSync('./sowpods.txt', 'utf-8')
+const chars = text.split('')//this logs 'Q' but when I do ('\n') it logs 'Z'
+
 // Which of the letters Q, X, and Z is the least common?
 
-const fs = require('fs')
+function solution(chars) {
+  let result = []
 
-const data = fs.readFileSync('./sowpods.txt', 'utf-8')
-const wordList = data.split('\n')
+  const counters = {
+    Q: 0,
+    X: 0,
+    Z: 0
+  }
+
+  chars.forEach(char => {
+    if (char === 'Q') counters.Q++
+    else if (char === 'X') counters.X++
+    else if (char === 'Z') counters.Z++
+  })
+
+  result = Object.keys(counters).reduce((a, b) => counters[a] < counters[b] ? a : b)
+
+  return result
+}
+
+console.log(solution(chars))
 
 
 
-function solution (words) {
-    words.forEach(function(word) {
-        if (!word.includes('Q')) {
-            console.log(word.count())
-        }
-    })
-} 
 
-solution(wordList)
+
+
+
+// function solution (words) {
+//     words.forEach(function(word) {
+//         if (!word.includes('Q')) {
+//             console.log(word.count())
+//         }
+//     })
+// } 
+
+// solution(wordList)
